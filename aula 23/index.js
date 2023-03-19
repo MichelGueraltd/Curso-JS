695aea937bd052e30ff41a1e1b43aeeb2118e0f4
MichelGueraldt <michelgueraldt@gmail.com>
2023-03-19 13:03:23 -0300

    diferenças entre (Var,Let)

---
+++ aula 23/index.js
@@ -0,0 +1,32 @@
+// DIFERENÇAS ENTRE (VAR.LET)
+// (Let) tem escopo de bloco {... bloco 
+// (Var) só tem escopo de função}
+
+//const verdadeira = true;
+
+//let nome = 'Luiz';
+//var nome2 = 'Luiz';
+
+//if (verdadeira) {
+//  let nome = 'Otavio'; // criando
+//var nome2 = 'Rogério'; // redeclarando
+
+//    if (verdadeira) {
+//        var nome2 = 'Ronaldo'; // redeclarando
+//        let nome = 'Outra coisa';
+//    }
+//}
+
+//console.log(nome, nome2);
+
+// função Var não respeita outros blocos, fora de funções.
+
+//function falaOi () {
+
+//if (verdadeira) {
+//let nome = 'Luiz';
+//var sobrenome = 'Miranda';
+//}
+//console.log(sobrenome);
+//}
+//falaOi ();
