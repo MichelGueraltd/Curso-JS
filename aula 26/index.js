5072e388bc79b7cdeae1695ab42105425bb5f175
MichelGueraldt <michelgueraldt@gmail.com>
2023-03-20 17:08:06 -0300

    For Clássico - (Estrutura de repetição)

---
+++ aula 26/index.js
@@ -0,0 +1,6 @@
+// For - Clássico - Estrutura de repetição
+
+const frutas = ['Maça', 'Pêra', 'Uva','Luiz', 'Otávio', 'Joã','Miranda'];
+for (let i = 0; i < frutas.length; i++){
+    console.log(`Indice ${i}`, frutas[i]);
+}
