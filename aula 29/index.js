b895b01ed1f632e78e8c1cd7cb16d6ae99bb93a7
MichelGueraldt <michelgueraldt@gmail.com>
2023-03-25 13:27:00 -0300

    For (of) Estrutr=uras de repetição

---
+++ aula 29/index.js
@@ -0,0 +1,31 @@
+// For (of) Estrutura de repetição
+
+//       (String)
+//const nome = 'Michel Cabral';
+//for (let valor of nomes){
+//    console.log(valor);
+//}
+
+//       (Arrays)
+const nomes = ['Michel', 'Souza', 'Cabral'];
+for (let valor of nomes){
+    console.log(valor);
+}
+
+//for (let i = 0; i < nome.length; i++){
+//console.log(nome[i]);
+//}
+
+//for (let i in nome){
+//console.log(nome[i]);
+//}
+
+//for (let valor of nome) {
+//  console.log(valor);
+//}
+
+
+//            EXPLICAÇÕES :
+// For Clássico - Geralmente com interáveis (arrays ou Strings)
+// For in - Retorna o índice ou chave (String, arrays ou objetos)
+// For of - Retotorna o valor em si (interáveis, arrays ou Strings)
