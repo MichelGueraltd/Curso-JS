c6e7d3ed4e4c7be19863543b8abd8bc1234fd5ab
MichelGueraldt <michelgueraldt@gmail.com>
2023-04-25 18:39:19 -0300

    Funções de callback

---
+++ aula 46/index.js
@@ -0,0 +1,55 @@
+//Funções de callback
+//Coisas que eu posso chamar quando alguma cosa terminar de ocorrer
+
+function rand(min = 1000, max = 3000) {
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
+}
+
+function f1(callback) {
+    setTimeout(function () {
+        console.log('f1');
+        if (callback) callback();
+    }, rand());
+
+}
+function f2(callback) {
+    setTimeout(function () {
+        console.log('f2');
+        if (callback) callback();
+
+    }, rand());
+}
+function f3(callback) {
+    setTimeout(function () {
+        console.log('f3');
+        if (callback) callback();
+
+    }, rand());
+}
+
+//TIPO 1
+
+//f1(function () {
+//  f2(function () {
+//    f3(function () {
+//            console.log('Ola Mundo!');
+//
+//        });
+//    });
+//});
+
+//TIPO 2
+
+f1(f1Callback);
+
+function f1Callback() {
+    f2(f2Callback);
+}
+
+function f2Callback() {
+    f3(f3Callback);
+}
+function f3Callback() {
+    console.log('Ola Mundo!');
+}
