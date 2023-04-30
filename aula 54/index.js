576bbdfefa456be46311a3d7ed59b7bda048e669
MichelGueraldt <michelgueraldt@gmail.com>
2023-04-30 13:51:40 -0300

    Revisão do Basico em Arrays

---
+++ aula 54/index.js
@@ -0,0 +1,20 @@
+// Para adicionar no Final
+//nomes.push('João');
+//nomes.push('Wallace');
+
+// Para adionar no Começo
+//nomes.unshift('João');
+//nomes.unshift('Wallace');
+//console.log(nomes);
+
+// const nomes = ['Eduardo', 'Maria','Joana','Wallace', 'Rosana'] ;
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+const nomes = [ 'Luiz', 'Otávio', 'Miranda' ];
+const nome = nomes.join(' ');
+console.log(nome);
+
+
+
+
