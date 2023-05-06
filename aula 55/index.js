127c76f055dd7600b6bc572bb633ecf35c60e119
MichelGueraldt <michelgueraldt@gmail.com>
2023-05-06 13:45:16 -0300

    Método Splice

---
+++ aula 55/index.js
@@ -0,0 +1,22 @@
+// Método Splice
+// negativos    -5       -4       -3        -2       -1         
+//  positvos     0        1        2         3        4
+//const nomes = ['Maria', 'João', 'Eduardo','Gabriel','Julia'];
+//.splice                   ídice delete
+//const removidos = nomes.splice(3, 2, 'Luiz');
+
+//pop
+// const removidos = nomes.splice(-1, 1);
+
+//shift
+//const removidos = nomes.splice(0, 1);
+
+//push
+const nomes = ['Maria','João','Eduardo','Gabriel','Júlia'];
+
+nomes.splice(0, 0, 'Luiz', 'Otávio' );
+console.log(nomes);
+
+
+
+
