2708bef615a83f2ebbd6488a284e73ddfd526366
MichelGueraldt <michelgueraldt@gmail.com>
2023-05-16 19:32:51 -0300

    Métodos úteis para objetos

---
+++ aula 65/index.js
@@ -0,0 +1,23 @@
+/* 
+Object.values
+Object.entries
+Object.assign(des, any)
+Object.getOwnPropertyDescriptor(o,'prop')
+... (spread)
+Já vimos
+Object.keys (retorna as chaves)
+Object.freeze (congela o objeto)
+Object.defineProperty (define uma propriedade)
+*/
+
+//Copiar um Objeto para outro Objeto!
+
+// const produto = { nome: 'Produto', preco: 1.8 };
+// const caneca = Object.assign({}, produto, {material : 'Porcelana'});
+
+// caneca.nome = 'Outro nome';
+// caneca.preco = 2.5;
+// console.log(produto);
+// console.log(caneca);
+
+
